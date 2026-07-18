31a5315baa7072441821a941a1abc104493d0898
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-07-18 22:37:34 +0545

    Time: 0 ms (100%) | Memory: 16.3 MB (31.74%) - LeetCommit

--- Algorithms/2106-Find-Greatest-Common-Divisor-of-Array/Find-Greatest-Common-Divisor-of-Array.cpp
+++ Algorithms/2106-Find-Greatest-Common-Divisor-of-Array/Find-Greatest-Common-Divisor-of-Array.cpp
@@ -3,8 +3,6 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        int min = *std::min_element(nums.begin(), nums.end());
-        int max = *std::max_element(nums.begin(), nums.end());
-        return std::gcd(min, max);
+        return std::gcd(*std::min_element(nums.begin(), nums.end()), *std::max_element(nums.begin(), nums.end()));
     }
 };
