2ac7e93bfcb21a313abbcc6884cf5399d249def8
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-07-18 23:21:58 +0545

    Time: 0 ms (100%) | Memory: 8.2 MB (74.18%) - LeetCommit

--- Algorithms/1212-Sequential-Digits/Sequential-Digits.cpp
+++ Algorithms/1212-Sequential-Digits/Sequential-Digits.cpp
@@ -1,25 +1,31 @@
+#include <vector>
+
 class Solution {
 public:
-    vector<int> sequentialDigits(int low, int high) {
-        vector<int> result;
-        vector<int> temp = {12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789};
-        int eleven = 11;
-        int current;
+    std::vector<int> sequentialDigits(int low, int high) {
+        std::vector<int> result;
+        
+        std::vector<int> temp = {12, 123, 1234, 12345, 123456, 1234567, 12345678, 123456789};
+        
+        int increment = 11;
         int consecutive = 8;
-        for (int i = 0; i < temp.size(); i++){
-            current = temp[i];
-            for (int j = 0; j < consecutive; j++){
-                if (current >= low && current<=high){
+        
+        for (int i = 0; i < temp.size(); i++) {
+            int current = temp[i];
+            
+            for (int j = 0; j < consecutive; j++) {
+                if (current >= low && current <= high) {
                     result.push_back(current);
-                }   
-                else if(current>=high){
-                    return result;
                 }
-                current += eleven;         
+                else if (current > high) {
+                    return result; 
+                }
+                
+                current += increment;
             }
+            
             consecutive--;
-            eleven*=10;
-            eleven++;
+            increment = increment * 10 + 1;
         }
         return result;
     }
