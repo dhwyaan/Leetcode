b37233725103b3db6600b8b7fe770b41d7bfc1e1
dhwyaan <104495203+dhwyaan@users.noreply.github.com>
2026-07-20 07:59:25 +0545

    Time: 0 ms (100%) | Memory: 18.2 MB (66.35%) - LeetCommit

--- Algorithms/1386-Shift-2D-Grid/Shift-2D-Grid.cpp
+++ Algorithms/1386-Shift-2D-Grid/Shift-2D-Grid.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        vector<int> temp;
-        for (vector<int> i: grid){
-            for (int j: i){
-                temp.push_back(j);
-            }
-        }
         int rows = grid.size();
         int cols = grid[0].size();
         int elem = rows*cols;
-        k = k%elem;
-        for (int i = 0; i<k; i++){
-            int x = temp[elem-1];
-            for (int j = elem - 1; j>0; j--){
-                temp[j] = temp[j-1];
+        k = k % elem;
+
+        vector<vector<int>> answer = grid;
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                int index = (i * cols + j - k + rows * cols) % (rows * cols);
+                int oldRow = index / cols;
+                int oldCol = index % cols;
+
+                answer[i][j] = grid[oldRow][oldCol];
             }
-            temp[0] = x;
         }
-        for (int i = 0; i<rows; i++){
-            for (int j = 0; j<cols;j++){
-                grid[i][j] = temp[i*cols+j];
-            }
-        }
-        return grid;
+        return answer;
     }
 };
